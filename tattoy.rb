98a9306885ed7d27dd9980405f5c9144330560d2
Thomas Buckley-Houston <tom@tombh.co.uk>
2025-06-12 22:35:35 -0300

    Initial commit

---
+++ tattoy.rb
@@ -0,0 +1,7 @@
+class Tattoy < Formula
+  desc "Text-based terminal compositor"
+  homepage "https://tattoy.sh"
+  version "0"
+  url "https://github.com/tattoy-org/tattoy/releases/download/v0/tattoy-aarch64-apple-darwin.tar.gz"
+  sha256 "5b42e7b039b7e98d4edc7ec7ec4146f9f790a965892e19b77b5bd2a17adc7932"
+end
