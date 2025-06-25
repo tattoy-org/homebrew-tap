27ef459f1202670757e59abc07ffda189141884e
Thomas Buckley-Houston <tom@tombh.co.uk>
2025-06-25 16:33:34 -0300

    tattoy 0.1.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- tattoy.rb
+++ tattoy.rb
@@ -1,9 +1,9 @@
 class Tattoy < Formula
   desc "Text-based terminal compositor"
   homepage "https://tattoy.sh"
-  version "0.1.2"
-  url "https://github.com/tattoy-org/tattoy/releases/download/v0.1.2/tattoy-aarch64-apple-darwin.tar.gz"
-  sha256 "80bb62cdfefd3d1440efd4ed29025c3b9aa9810fd36b1a2a95f0e197be77c267"
+  version "0.1.3"
+  url "https://github.com/tattoy-org/tattoy/releases/download/v0.1.3/tattoy-aarch64-apple-darwin.tar.gz"
+  sha256 "821f410decf71f6b10a105c0a32e58a66e380d508d04b0f7edaa478d3d2bd711"
   
   def install
     bin.install "tattoy"
